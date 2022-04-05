c14b8b2198fa398e32396b60c441de870e12edab
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-04-05 10:51:15 -0600

    Uso de Switch

--- calculadora.go
+++ calculadora.go
@@ -13,7 +13,8 @@ func main() {
 	scanner.Scan()
 	operacion := scanner.Text()
 	fmt.Println(operacion)
-	valores := strings.Split(operacion, "+")
+	operador := "/"
+	valores := strings.Split(operacion, operador)
 	fmt.Println(valores)
 	fmt.Println(valores[0] + valores[1])
 	operador1, err1 := strconv.Atoi(valores[0])
@@ -25,4 +26,16 @@ func main() {
 	operador2, _ := strconv.Atoi(valores[1])
 	fmt.Println(operador1 + operador2)
 
+	switch operador {
+	case "+":
+		fmt.Println(operador1 + operador2)
+	case "-":
+		fmt.Println(operador1 - operador2)
+	case "*":
+		fmt.Println(operador1 * operador2)
+	case "/":
+		fmt.Println(operador1 / operador2)
+	default:
+		fmt.Println(operador, "El operador no esta soportado")
+	}
 }
