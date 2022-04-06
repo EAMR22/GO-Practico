0205e124a5caadc26a92cd294ae9e02fec3e5e79
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-04-06 15:55:40 -0600

    punteros en Structs

--- tareas.go
+++ tareas.go
@@ -8,24 +8,24 @@ type task struct {
 	completado  bool
 }
 
-func (t task) marcarCompleta() {
+func (t *task) marcarCompleta() { // Utiliza el * para actualizar el nuevo valor que viene de la referencia.
 	t.completado = true
 }
 
-func (t task) actualizarDescripcion(descripcion string) {
+func (t *task) actualizarDescripcion(descripcion string) {
 	t.descripcion = descripcion
 }
 
-func (t task) actualizarNombre(nombre string) {
+func (t *task) actualizarNombre(nombre string) {
 	t.nombre = nombre
 }
 
 func main() {
-	t := task{
+	t := &task{ // Ya no enviamos una copia sino una referencia del struct que creamos.
 		nombre:      "Completar mi curso de go",
 		descripcion: "Completar mi curso de go de Platzi esta semana",
 	}
-	fmt.Printf("%+v\n", t) // Con Printf pone un formato a lo que se va a imprimir.
+	fmt.Printf("%+v\n", t)
 
 	t.marcarCompleta()
 	t.actualizarDescripcion("Finalizar mi curso de go")
