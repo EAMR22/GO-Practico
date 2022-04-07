059b2ea2e48c5d4b8f08dc4695ef536cb7348516
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-04-07 00:24:40 -0600

    ciclo for

--- tareas.go
+++ tareas.go
@@ -6,12 +6,12 @@ type taskList struct {
 	tasks []*task
 }
 
-func (t *taskList) agregarALista(tl *task) { // Lo enviamos con * para evitar hacer modificaciones a valores copia.
-	t.tasks = append(t.tasks, tl) // El 1 parametro toma el slice y el 2 parametro toma el valor que se agregara al slice.
+func (t *taskList) agregarALista(tl *task) {
+	t.tasks = append(t.tasks, tl)
 }
 
 func (t *taskList) eliminarDeLista(index int) {
-	t.tasks = append(t.tasks[:index], t.tasks[index+1:]...) // Los ... es porque el 2 parametro no es un slice.
+	t.tasks = append(t.tasks[:index], t.tasks[index+1:]...)
 }
 
 type task struct {
@@ -54,9 +54,27 @@ func main() {
 		},
 	}
 
-	fmt.Println(lista.tasks[0])
 	lista.agregarALista(t3)
-	fmt.Println(len(lista.tasks)) // Imprime 3
-	lista.eliminarDeLista(1)
-	fmt.Println(len(lista.tasks)) // Imprime 2
+
+	for i := 0; i < len(lista.tasks); i++ {
+		fmt.Println("Index", i, "Nombre", lista.tasks[i].nombre)
+	}
+
+	for index, tarea := range lista.tasks { // range devuelve 2 valores y a la vez crea un iterador que recorre el ciclo.
+		fmt.Println("Index", index, "Nombre", tarea.nombre)
+	}
+
+	for i := 0; i < 10; i++ {
+		if i == 5 {
+			break // break lo que hace es parar el ciclo en esa condicion.
+		}
+		fmt.Println(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		if i == 5 {
+			continue // Simplemente rompe la iteracion, pero el ciclo continua.
+		}
+		fmt.Println(i)
+	}
 }
