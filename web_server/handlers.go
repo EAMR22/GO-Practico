330218999ee93c1d4084d2b7aa67a287d3d9ea2c
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-04-11 13:42:48 -0600

    Respondiendo Request JSON

--- web_server/handlers.go
+++ web_server/handlers.go
@@ -22,5 +22,22 @@ func PostRequest(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "error: %v", err)
 		return
 	}
-	fmt.Fprint(w, "Payload %v\n", metadata)
+	fmt.Fprintf(w, "Payload %v\n", metadata)
+}
+
+func UserPostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	response, err := user.ToJson()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "aplication/json")
+	w.Write(response)
 }
